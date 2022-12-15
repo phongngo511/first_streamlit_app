f2ded5a14cc27520825bb38c395720a370e7a6d2
phongngo511 <83492792+phongngo511@users.noreply.github.com>
2022-12-16 10:51:40 +1100

    Update streamlit_app.py
    Added colon after else statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
-   else 
+   else:
       fruitvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
       fruitvice_normalized = pandas.json_normalize(fruityvice_response.json())
       streamlit.dataframe(fruityvice_normalized)
